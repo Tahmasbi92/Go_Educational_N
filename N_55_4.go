cfffe54ff70c29bfa897722ab6bc284f1a2ad51a
tahmasbi92 <atemaaaa92@gmail.com>
2025-03-17 23:18:25 +0330

    create file (Generics)

--- N_55_4.go
+++ N_55_4.go
@@ -2,7 +2,9 @@ package main
 
 import "fmt"
 
-func Print[T string | int | float64](value T) T {
+func Print[T string | int | float64](value T) T { // be jaye estefade az "any" ke shamele
+	// tamame vorudiha (string , int , float , bool) mishavad mitavan az in dasture bala ba tavajoh be
+	//func main payeen estafade kard.
 	fmt.Println(value)
 
 	return value
